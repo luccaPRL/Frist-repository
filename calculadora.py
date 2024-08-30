739b2b4e4cc15f8ccdb8b940d3a1004b27063e78
Lucca Pereira Locambo De Oliveira <luccalocambopereira@gmail.com>
2024-08-30 10:12:46 -0300

    calculadora commit 1

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,25 @@
-1 = print("digite seu numero")
-2 = print("digite o segundo numero")
+numero1 = 0
+numero2 = 0
+resultado = 0
+operação = ""
 
-print(1+2)
+
+numero1 = int(input("digite o numero 1:"))
+operação = input("digite a operação:")
+numero2 = int(input("digite o numero 2:"))
+
+
+if operação == "+":
+    resultado = numero1 + numero2
+elif operação == "-":
+    resultado = numero1 - numero2
+elif operação == "/":
+    resultado = numero1 / numero2
+elif operação == "*":
+    resultado = numero1 * numero2
+elif operação == "**":
+    resultado = numero1 ** numero2
+else:
+    resultado = "operação invalida"
+
+print(str(numero1) + "" + str(operação) + "" + str(numero2) +  "=" + str(resultado))
