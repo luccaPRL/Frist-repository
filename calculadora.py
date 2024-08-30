718ab5f3398bd90bf071cd070d11c77ca4a76efc
Lucca Pereira Locambo De Oliveira <luccalocambopereira@gmail.com>
2024-08-30 10:19:54 -0300

    calculadora commit 2

--- calculadora.py
+++ calculadora.py
@@ -3,23 +3,22 @@ numero2 = 0
 resultado = 0
 operação = ""
 
+while True:
+    numero1 = int(input("digite o numero 1:"))
+    operação = input("digite a operação:")
+    numero2 = int(input("digite o numero 2:"))
 
-numero1 = int(input("digite o numero 1:"))
-operação = input("digite a operação:")
-numero2 = int(input("digite o numero 2:"))
+    if operação == "+":
+        resultado = numero1 + numero2
+    elif operação == "-":
+        resultado = numero1 - numero2
+    elif operação == "/":
+        resultado = numero1 / numero2
+    elif operação == "*":
+        resultado = numero1 * numero2
+    elif operação == "**":
+        resultado = numero1 ** numero2
+    else:
+        resultado = "operação invalida"
 
-
-if operação == "+":
-    resultado = numero1 + numero2
-elif operação == "-":
-    resultado = numero1 - numero2
-elif operação == "/":
-    resultado = numero1 / numero2
-elif operação == "*":
-    resultado = numero1 * numero2
-elif operação == "**":
-    resultado = numero1 ** numero2
-else:
-    resultado = "operação invalida"
-
-print(str(numero1) + "" + str(operação) + "" + str(numero2) +  "=" + str(resultado))
+    print(str(numero1) + "" + str(operação) + "" + str(numero2) + "=" + str(resultado))
